28d2cab8b0f97b6418edf38b4a615ca0e2a692e7
Élisson Michael <elissonmichael@gmail.com>
2021-04-14 17:53:43 -0300

    Change ServerSide to StaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
 import styles from '../styles/Home.module.css'
 
-function Home({ data }) {
+export default function Home(props) {
   return (
     <div>
       <Head>
@@ -11,7 +11,7 @@ function Home({ data }) {
       <main className={styles.main}>
       <h1> Notices </h1>
         {
-          data.map(notice =>
+          props.notices.map(notice =>
             <>
               <h3>
                 { notice.title }
@@ -27,11 +27,13 @@ function Home({ data }) {
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps() {
   const res = await fetch(`https://notice-api.herokuapp.com/notices`);
   const data = await res.json();
 
-  return { props: { data } }
+  return {
+    props: {
+      notices: data
+    }
+  }
 }
-
-export default Home
