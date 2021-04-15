7dd0c30cadee37f43c46a1d7259253da88f61dcd
Élisson Michael <elissonmichael@gmail.com>
2021-04-15 19:38:12 -0300

    Fix Child in a List Should Have a Unique "key" Warning

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,19 @@ export default function Home(props) {
     <Title> Notices </Title>
     <div className='row'>
       <div className='input-field col m4 offset-m8'>
-        <Link href='notices/new'>
-          <a class="waves-effect waves-light btn">Create Notice</a>
+        <Link href="/notices/new">
+          <a className="waves-effect waves-light btn">Create Notice</a>
         </Link>
       </div>
     </div>
-    <ul class="collapsible">
+    <ul className="collapsible">
     {
       props.notices.map(notice =>
-        <li>
-          <div class="collapsible-header">
-            <b> ID: { notice.id } </b> <i class="material-icons">swap_vert</i>{ notice.title }
+        <li key={ notice.id }>
+          <div className="collapsible-header">
+            <b> ID: { notice.id } </b> <i className="material-icons">swap_vert</i>{ notice.title }
           </div>
-          <div class="collapsible-body"><span>{ notice.description }</span></div>
+          <div className="collapsible-body"><span>{ notice.description }</span></div>
         </li>
       )
     }
