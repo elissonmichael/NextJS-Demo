ae0838f6ac5616882ea7c1cd647248c544a11a1c
Élisson Michael <elissonmichael@gmail.com>
2021-04-15 19:49:28 -0300

    Add React Hook & Form Validation

--- components/NoticeForm.js
+++ components/NoticeForm.js
@@ -1,29 +1,39 @@
 import Link from 'next/link';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export default function NoticeForm() {
+export default function NoticeForm({onSubmit}) {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   return (
-    <form className="col s12">
+    <form className="col s12" onSubmit={handleSubmit(onSubmit)}>
       <div className="row">
         <div className="input-field col s12">
-          <input id="title" type="text" className="validate"/>
+          <input {...register('title', { required: true })} id="title" type="text" className="validate"/>
           <label htmlFor="title">Title</label>
+          { errors.title &&
+            <span className="helper-text" data-error="wrong">Title can't be blank</span>
+          }
         </div>
       </div>
       <div className="row">
         <div className="input-field col s12">
-          <textarea id="description" className="materialize-textarea"></textarea>
+          <textarea {...register('description', { required: true })} id="description" className="materialize-textarea"></textarea>
           <label htmlFor="description">Description</label>
+          { errors.description &&
+            <span className="helper-text" data-error="wrong">Description can't be blank</span>
+          }
         </div>
       </div>
-      <div class='row'>
-        <div class='col s6 right-align'>
+      <div className='row'>
+        <div className='col s6 right-align'>
           <button className='waves-effect waves-light btn deep-purple darkn-4' type='submit'>
             Save
           </button>
         </div>
-        <div class='col s6 left-align'>
+        <div className='col s6 left-align'>
           <Link href='/'>
-            <a class="waves-effect waves-light btn">Cancel</a>
+            <a className="waves-effect waves-light btn">Cancel</a>
           </Link>
         </div>
       </div>
--- pages/notices/new.js
+++ pages/notices/new.js
@@ -1,11 +1,15 @@
 import NoticeForm from '../../components/NoticeForm'
 import Title from '../../components/Title';
 
+function createNotice(data) {
+  console.log(data);
+}
+
 export default function NoticeNew() {
   return (
     <>
       <Title> Register Notice </Title>
-      <NoticeForm />
+      <NoticeForm onSubmit={createNotice}/>
     </>
   )
 };
