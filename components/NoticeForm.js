32522188815cb20ceb65dae3e3df9f1dc208a340
Élisson Michael <elissonmichael@gmail.com>
2021-04-15 18:42:08 -0300

    Fix Notice Form Parenting

--- components/NoticeForm.js
+++ components/NoticeForm.js
@@ -2,33 +2,31 @@ import Link from 'next/link';
 
 export default function NoticeForm() {
   return (
-    <>
-    <div className="row">
-      <div className="input-field col s12">
-        <input id="title" type="text" className="validate"/>
-        <label for="title">Title</label>
-      </div>
-    </div>
-    <div className="row">
-      <form className="col s12">
-        <div className="row">
-          <div className="input-field col s12">
-            <textarea id="description" className="materialize-textarea"></textarea>
-            <label for="description">Description</label>
-          </div>
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="title" type="text" className="validate"/>
+          <label htmlFor="title">Title</label>
         </div>
-      </form>
-    </div>
-    <div class='row'>
-      <div class='col s6 right-align'>
-        <a class="waves-effect waves-light btn deep-purple darkn-4">Save</a>
       </div>
-      <div class='col s6 left-align'>
-        <Link href='/'>
-          <a class="waves-effect waves-light btn">Cancel</a>
-        </Link>
+      <div className="row">
+        <div className="input-field col s12">
+          <textarea id="description" className="materialize-textarea"></textarea>
+          <label htmlFor="description">Description</label>
+        </div>
       </div>
-    </div>
-    </>
+      <div class='row'>
+        <div class='col s6 right-align'>
+          <button className='waves-effect waves-light btn deep-purple darkn-4' type='submit'>
+            Save
+          </button>
+        </div>
+        <div class='col s6 left-align'>
+          <Link href='/'>
+            <a class="waves-effect waves-light btn">Cancel</a>
+          </Link>
+        </div>
+      </div>
+    </form>
   )
 };
